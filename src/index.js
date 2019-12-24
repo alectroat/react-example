833bcac50124a8a39024d3b95ebbb9eda9347ca9
saikat shill <saikatshill19@gmail.com>
2019-12-24 14:05:18 +0600

    push all

--- src/index.js
+++ src/index.js
@@ -1,12 +1,74 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { Route, Link, BrowserRouter as Router } from 'react-router-dom';
+import App from './components/app';
+import Users from './components/users';
+import Contact from './components/contact';
+import Form from './components/form';
+import ToDoList from './components/fetch/todolist';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import Parent from './components/showlist/parent';
+import ReduxApp from './components/ReduxApp';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+
+import todoApp from './reducers/reducers'
+
+let store = createStore(todoApp)
+let rootElement = document.getElementById('root')
+
+export default class Main extends Component {
+    render() {
+        return (
+            <Router>
+            <div>
+                <div className="leftMenu">
+                    <ul>
+                        <li>
+                            <Link to="/">Home</Link>
+                        </li>
+                        <li>
+                            <Link to="/users">Users</Link>
+                        </li>
+                        <li>
+                            <Link to="/contact">Contact</Link>
+                        </li>
+                        <li>
+                            <Link to="/form">Form</Link>
+                        </li>
+                        <li>
+                            <Link to="/list">List View</Link>
+                        </li>
+                        <li>
+                            <Link to="/redux">Redux</Link>
+                        </li>
+                        <li>
+                            <Link to="/todolist">To Do List</Link>
+                        </li>
+                    </ul>
+                </div>
+                <div className="rightMenu">
+                    <Route exact path="/" component={App} />
+                    <Route path="/users" component={Users} />
+                    <Route path="/contact" component={Contact} />
+                    <Route path="/form" component={Form} />
+                    <Route path="/list" component={Parent} />
+                    <Route path="/redux" component={ReduxApp} />
+                    <Route path="/todolist" component={ToDoList} />
+                </div>
+            </div>
+            </Router>
+        );
+    }
+}
+
+ReactDOM.render(
+    <Provider store={store}>
+        <Main />
+    </Provider>,
+    rootElement
+)
+
+//ReactDOM.render(<Main/>, document.getElementById('root'))
